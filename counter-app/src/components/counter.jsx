33829bfa83ec964900b2ab4ec0ed8ec6fc8a5f2c
Tom Busby <tom@busby.ninja>
2018-10-03 16:35:02 +0200

    Bind this to handleIncrement using constructor in Counter

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,11 +6,25 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement() {
+    console.log("Increment clicked", this);
+  }
+
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.renderTags()}
       </React.Fragment>
     );
